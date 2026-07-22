22dbc4f4095dafacd05c7fdf65b8ae47c56df56a
jordenric96 <jordenricour96@gmail.com>
2026-07-22 19:25:53 +0000

    Echte Supabase API sleutel toegevoegd

--- js/database.js
+++ js/database.js
@@ -1,7 +1,7 @@
 // js/database.js
 
 const supabaseUrl = 'https://eyymmsqitfshqbkcjsqy.supabase.co'; 
-const supabaseKey = 'sb_publishable_u-2UeA3zqbjavdx37ul1UQ_hb5Z5fQr.'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5eW1tc3FpdGZzaHFia2Nqc3F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MzgyOTIsImV4cCI6MjEwMDMxNDI5Mn0.ayDIi3v6LDi2IuZJWR3zBS8w-hq1_LlkwbbXYvuGgj4'; 
 
 // Initialiseer de Supabase client
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
